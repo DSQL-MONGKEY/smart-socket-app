f92d69e00722bb67eb8b2419aafb02549e073d24
Dimpfe <dimasprasetyo7696@gmail.com>
2025-07-04 23:16:30 +0700

    feat<>: initial create

---
+++ app/notifications.tsx
@@ -0,0 +1,10 @@
+import Screen from '@/components/ui/layout/Screen';
+import { Text } from 'react-native';
+
+export default function NotificationsScreen() {
+  return (
+    <Screen className="items-center justify-center">
+      <Text className="text-xl font-semibold">Notifikasi</Text>
+    </Screen>
+  );
+}
