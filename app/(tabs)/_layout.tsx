bf2a95fb55dcd6344245b354c80f4fb3414f17f5
Dimpfe <dimasprasetyo7696@gmail.com>
2025-07-04 23:17:19 +0700

    feat<>: add initial page to index and add screenOptions and map the navItems

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,27 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, View } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
+import { navItems } from '@/constants/Data';
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+function TabBarIcon({ icon }: { icon: React.ReactNode }) {
+  return (
+    <View className='flex'>
+      {icon}
+    </View>
+  );
+}
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <Tabs
+      initialRouteName="index"
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
@@ -23,23 +32,34 @@ export default function TabLayout() {
             // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
           },
+          android: {
+            position: 'absolute',
+            bottom: 20,
+            elevation: 0,
+            backgroundColor: '#fff',
+            borderRadius: 16,
+            height: 60,
+            tabBarLabelStyle: {
+              fontSize: 12,
+            },
+          },
           default: {},
         }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+      }}
+
+      >
+        {navItems.map(({ name, label, icon, isActive }) => (
+          <Tabs.Screen
+            key={name}
+            name={name}
+            options={{
+              title: label,
+              tabBarIcon: () => (
+                <TabBarIcon icon={icon} />
+              ),
+            }}
+          />
+        ))}
     </Tabs>
   );
 }
