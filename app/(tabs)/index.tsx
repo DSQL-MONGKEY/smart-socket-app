f8f159391fdf8dc964b6270665881cedeff67a45
Dimpfe <dimasprasetyo7696@gmail.com>
2025-07-05 20:09:12 +0700

    feat<>: add colorSchema for color conditioning

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,36 +4,39 @@ import { ThemedView } from '@/components/ThemedView';
 import ScrollScreen from '@/components/ui/layout/ScrollScreen';
 import { useRouter } from 'expo-router';
 import { Bell } from 'lucide-react-native';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, useColorScheme } from 'react-native';
 
 export default function HomeScreen() {
   const router = useRouter();
+  const colorSchema = useColorScheme();
 
   return (
     <ScrollScreen className='px-5'>
-        <ThemedView 
-          style={styles.container}
-          className="flex-row justify-between items-center py-6"
-        >
-          <ThemedView className='gap-2'>
-            <Text className='text-lg font-thin text-gray-500 dark:text-gray-400'>
-              Welcome Back!
-            </Text>
-            <Text className='text-4xl text-gray-700 dark:text-gray-300 font-bold'>
-              Eddy
-            </Text>
-          </ThemedView>
-
-          <TouchableOpacity 
-            onPress={() => router.push('/notifications')}
-            className='bg-slate-100 dark:bg-zinc-800 p-3 rounded-full items-center justify-center shadow'>
-              <Bell size={25} color={"#000"} />
-          </TouchableOpacity>
+      <ThemedView 
+        style={styles.container}
+        className="flex-row justify-between items-center py-6"
+      >
+        <ThemedView className='gap-2'>
+          <Text className='text-lg font-thin text-gray-500 dark:text-gray-400'>
+            Welcome Back!
+          </Text>
+          <Text className='text-4xl text-gray-700 dark:text-gray-300 font-bold'>
+            Eddy
+          </Text>
         </ThemedView>
 
-        <ThemedView className='flex-row mt-6 gap-2'>
-          <StatCard />
-        </ThemedView>
+        <TouchableOpacity 
+          onPress={() => router.push('/notifications')}
+          className='bg-slate-100 dark:bg-zinc-800 p-3 rounded-full items-center text-white justify-center shadow'>
+            <Bell 
+              size={25} 
+              color={colorSchema === 'dark' ? '#F6F6F6' : '#E67514'} />
+        </TouchableOpacity>
+      </ThemedView>
+
+      <ThemedView className='flex-row mt-6 gap-2'>
+        <StatCard />
+      </ThemedView>
     </ScrollScreen>
   );
 }
