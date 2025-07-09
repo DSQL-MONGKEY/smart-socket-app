38065cfc841243258849c4f4a43acf785c616daf
Dimpfe <dimasprasetyo7696@gmail.com>
2025-07-09 17:26:07 +0700

    feat<>: add props component

--- components/features/home/StatCard.tsx
+++ components/features/home/StatCard.tsx
@@ -1,55 +1,80 @@
-import { BadgeDollarSign, Unplug, Zap } from 'lucide-react-native';
+import { Activity, BadgeDollarSign, Unplug, Zap } from 'lucide-react-native';
 import { Text, View } from "react-native";
 
-export default function StatCard() {
+export interface StatCardProps {
+   arus: number;
+   biaya: number;
+   daya: number;
+   tegangan: number;
+}
+
+export default function StatCard({ arus, biaya, daya, tegangan }: StatCardProps) {
    return (
       <>
-         {/* Left Side */}
+         <View className='flex-row gap-2'>
+            {/* Left Side */}
+            <View className='flex-1 bg-slate-100 dark:bg-zinc-800 p-4 rounded-xl shadow'>
+               <Text className='text-gray-500 dark:text-gray-400 text-md'>
+                  Watt
+               </Text>
+               <View className='flex-row items-center gap-1'>
+                  <Activity size={15} color={"#999"} />
+                  <Text className='text-sm text-gray-500 dark:text-gray-400'>
+                     Currently power in use
+                  </Text>
+               </View>
+               <Text className='text-2xl font-semibold mt-5 text-gray-700 dark:text-gray-300'>
+                  {daya} Watt
+               </Text>
+            </View>
+
+            {/* Right Side */}
+            <View className='justify-between gap-2'>
+               <View className='w-48 bg-slate-100 dark:bg-zinc-800 p-4 rounded-xl shadow'>
+                  <Text className='text-sm text-gray-500 dark:text-gray-400'>
+                     Power Consumption
+                  </Text>
+                  <View className='flex-row items-center gap-1 mt-1'>
+                     <Zap size={15} color={"#999"} />
+                     <Text className='text-sm text-gray-500 dark:text-gray-400'>
+                     Voltage
+                     </Text>
+                  </View>
+                  <Text className='text-2xl font-semibold mt-2 text-gray-700 dark:text-gray-300'>
+                     {tegangan} V
+                  </Text>
+               </View>
+               <View className='w-48 bg-slate-100 dark:bg-zinc-800 p-4 rounded-xl shadow'>
+                  <Text className='text-sm text-gray-500 dark:text-gray-400'>
+                     Current in use
+                  </Text>
+                  <View className='flex-row items-center gap-1 mt-1'>
+                     <Unplug size={15} color={"#999"} />
+                     <Text className='text-sm text-gray-500 dark:text-gray-400'>
+                     Amphere
+                     </Text>
+                  </View>
+                  <Text className='text-2xl font-semibold mt-2 text-gray-700 dark:text-gray-300'>
+                     {arus} A
+                  </Text>
+               </View>
+            </View>
+         </View>
+
+         {/* bottom stat */}
          <View className='flex-1 bg-slate-100 dark:bg-zinc-800 p-4 rounded-xl shadow'>
             <Text className='text-gray-500 dark:text-gray-400 text-md'>
-               Cost KW/h
+               Cost
             </Text>
             <View className='flex-row items-center gap-1'>
                <BadgeDollarSign size={15} color={"#999"} />
                <Text className='text-sm text-gray-500 dark:text-gray-400'>
-                  Currently energy cost
+                  Cost of energy consumed
                </Text>
             </View>
             <Text className='text-2xl font-semibold mt-5 text-gray-700 dark:text-gray-300'>
-               Rp.12,500
+               Rp. {biaya}
             </Text>
-         </View>
-
-         {/* Right Side */}
-         <View className='justify-between gap-2'>
-            <View className='w-48 bg-slate-100 dark:bg-zinc-800 p-4 rounded-xl shadow'>
-               <Text className='text-sm text-gray-500 dark:text-gray-400'>
-                  Power Consumption
-               </Text>
-               <View className='flex-row items-center gap-1 mt-1'>
-                  <Zap size={15} color={"#999"} />
-                  <Text className='text-sm text-gray-500 dark:text-gray-400'>
-                  Voltage
-                  </Text>
-               </View>
-               <Text className='text-2xl font-semibold mt-2 text-gray-700 dark:text-gray-300'>
-                  0.00 V
-               </Text>
-            </View>
-            <View className='w-48 bg-slate-100 dark:bg-zinc-800 p-4 rounded-xl shadow'>
-               <Text className='text-sm text-gray-500 dark:text-gray-400'>
-                  Current AC/DC
-               </Text>
-               <View className='flex-row items-center gap-1 mt-1'>
-                  <Unplug size={15} color={"#999"} />
-                  <Text className='text-sm text-gray-500 dark:text-gray-400'>
-                  Amphere
-                  </Text>
-               </View>
-               <Text className='text-2xl font-semibold mt-2 text-gray-700 dark:text-gray-300'>
-                  0.00
-               </Text>
-            </View>
          </View>
       </>
    )
